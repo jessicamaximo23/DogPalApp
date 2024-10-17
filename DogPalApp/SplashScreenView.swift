41cce66981a2596429658ebb67b480215e94fe56
Jessica Maximo <jessicamaximo@Jessicas-MacBook-Air.local>
2024-10-17 13:55:29 -0400

    Start Project

---
+++ DogPalApp/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  DogPalApp
+//
+//  Created by Jessica Maximo on 2024-10-17.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
---
+++ DogPalAppTests/DogPalAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DogPalAppTests.swift
+//  DogPalAppTests
+//
+//  Created by Jessica Maximo on 2024-10-17.
+//
+
+import Testing
+@testable import DogPalApp
+
+struct DogPalAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

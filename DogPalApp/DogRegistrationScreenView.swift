fbc3b63a074a416144a2dca47dc5302f03ca081e
jessica maximo <jessicamaximo23@gmail.com>
2024-10-22 11:05:53 -0400

    fix ui btn bakn & btn cancel

--- DogPalApp/DogRegistrationScreenView.swift
+++ DogPalApp/DogRegistrationScreenView.swift
@@ -22,100 +22,115 @@ struct DogRegistrationScreenView: View {
        var body: some View {
            NavigationView {
                VStack {
-
-                   Image("DogPalLogo2")
-                       .resizable()
-                       .scaledToFit()
-                       .padding()
-                       .frame(width: 350, height: 150)
                    
-                   VStack(alignment: .leading) {
-                       Text("Set up your account")
-                           .font(.largeTitle)
-                           .padding(.bottom, 20)
-                   }
-                   // Set up Informations
-                   Form {
-                       TextField("Your Name", text: $userName)
-                       TextField("Dog's Name", text: $dogName)
-                       TextField("Dog's Breed", text: $dogBreed)
-                       TextField("Dog's Age", text: $dogAge)
-                       TextField("Dog's Size", text: $dogSize)
-                           .keyboardType(.numberPad)
+                       Image("DogPalLogo2")
+                           .resizable()
+                           .scaledToFit()
+                           .padding()
+                           .frame(width: 350, height: 150)
                        
-                       Section(header: Text("Introduce your best buddy to the community with a picture")) {
+                       VStack(alignment: .leading) {
+                           Text("Set up your account")
+                               .font(.largeTitle)
+                               .padding(.bottom, 20)
+                       }
+                       // Set up Informations
+                       Section {
+                            TextField("Your Name", text: $userName)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding(.horizontal)
                            
-                           if let image = dogPhoto {
+                            TextField("Dog's Name", text: $dogName)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding(.horizontal)
+                           
+                            TextField("Dog's Breed", text: $dogBreed)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding(.horizontal)
+                           
+                           TextField("Dog's Age", text: $dogAge)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding(.horizontal)
+                           
+                            TextField("Dog's Size", text: $dogSize)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .keyboardType(.numberPad)
+                            .padding(.horizontal)
+                           
+                           Section(header: Text("Introduce your best buddy to the community with a picture")) {
                                
-                               Image(uiImage: image)
-                                   .resizable()
-                                   .scaledToFit()
-                                   .frame(height: 200)
-                                   .clipShape(Circle())
-                                   .shadow(radius: 10)
-                               
-                           } else {
-                               Button(action: {
-                                   showImagePicker = true
-                               }) {
-                                   HStack {
-                                       Image(systemName: "camera")
-                                           .font(.largeTitle)
-                                           .foregroundColor(.brown)
-                                       
-                                       Text("Select a Photo")
-                                           .font(.headline)
-                                           .foregroundColor(.brown)
+                               if let image = dogPhoto {
+                                   
+                                   Image(uiImage: image)
+                                       .resizable()
+                                       .scaledToFit()
+                                       .frame(height: 200)
+                                       .clipShape(Circle())
+                                       .shadow(radius: 10)
+                                   
+                               } else {
+                                   Button(action: {
+                                       showImagePicker = true
+                                   }) {
+                                       HStack {
+                                           Image(systemName: "camera")
+                                               .font(.largeTitle)
+                                               .foregroundColor(.brown)
+                                           
+                                           Text("Select a Photo")
+                                               .font(.headline)
+                                               .foregroundColor(.brown)
+                                       }
                                    }
                                }
                            }
+                           
+                           //Submit dog
+                           Section {
+                               Button(action: {
+                                   let age = Int(dogAge) ?? 0
+                                   let size = Int(dogSize) ?? 0
+                                   
+                                   let newDog = Dog(name: dogName, breed: dogBreed, age: age, size: size)
+                                   
+                                   newDog.registerDog()
+                                   
+                                   
+                               }) {
+                                   Text("Submit your Dog")
+                                       .frame(maxWidth: .infinity)
+                                       .padding()
+                                       .background(Color.brown)
+                                       .foregroundColor(.white)
+                                       .cornerRadius(30)
+                               }
+                               .disabled(dogName.isEmpty || dogBreed.isEmpty || dogAge.isEmpty || dogPhoto == nil)
+                           }
                        }
                        
-                    //Submit dog
-                    Section {
-                        Button(action: {
-                            let age = Int(dogAge) ?? 0
-                            let size = Int(dogSize) ?? 0
-
-                            let newDog = Dog(name: dogName, breed: dogBreed, age: age, size: size)
-                            
-                                newDog.registerDog()
-                               
-                               
-                           }) {
-                        Text("Submit your Dog")
-                                   .frame(maxWidth: .infinity)
-                                   .padding()
-                                   .background(Color.brown)
-                                   .foregroundColor(.white)
-                                   .cornerRadius(30)
-                           }
-                           .disabled(dogName.isEmpty || dogBreed.isEmpty || dogAge.isEmpty || dogPhoto == nil)
-                       }
-                   }
                    
                }
-           }
-           .toolbar {
-                        ToolbarItem(placement: .navigationBarLeading) {
-                              Button(action: {
-                                  dismiss()
-                              }) {
-                                Label("Back", systemImage: "arrow.left")
-                              }
-                          }
-                        ToolbarItem(placement: .navigationBarTrailing) {
-                              Button(action: {
-                                  dismiss()
-                              }) {
-                                  Label("Cancel", systemImage: "xmark")
-                                      .foregroundColor(.blue)
-                              }
-                          }
-                      }
-                      .sheet(isPresented: $showImagePicker) {
-                          ImagePicker(image: $dogPhoto)
-                      }
+               .toolbar {
+                   ToolbarItem(placement: .navigationBarLeading) {
+                       Button(action: {
+                           dismiss()
+                       }) {
+                           Label("Back", systemImage: "arrow.left")
+                       }
+                   }
+                   ToolbarItem(placement: .navigationBarTrailing) {
+                       Button(action: {
+                           dismiss()
+                       }) {
+                           Label("Cancel", systemImage: "xmark")
+                               .foregroundColor(.blue)
+                       }
+                   }
+               }
+               .sheet(isPresented: $showImagePicker) {
+                   ImagePicker(image: $dogPhoto)
+               }
+                }
                   }
               }
           
@@ -132,9 +147,10 @@ class Dog {
         self.dogAge = age
         self.dogSize = size
     }
+    
 func registerDog() {
     
-           print("Registered Dog: \(dogName), \(dogBreed), \(dogAge)")
+           print("Registered Dog: \(dogName), \(dogBreed), \(dogAge), Size: \(dogSize)")
        }
 }
    

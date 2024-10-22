ebab2dcfdf1bcfa3085f431798528fc7b7fecfbc
Julien <villantijulien@gmail.com>
2024-10-21 21:32:24 -0400

    Added Firebase init

--- DogPalApp/DogPalAppApp.swift
+++ DogPalApp/DogPalAppApp.swift
@@ -6,9 +6,17 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseCore
+import FirebaseAuth
+
 
 @main
 struct DogPalAppApp: App {
+    init(){
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             SplashScreenView()

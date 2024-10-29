8633de32a86970cb9ff9208e29907769b37a3e3d
Wandrey Haagensen <wandrey.wic@gmail.com>
2024-10-29 14:39:20 -0400

    Commiting walker profile creation. Hard coded information to be show on walking booking (just for presentation reasons on thursday, to be changed later). "Experience" is not being sent for Booking page for some reason...

---
+++ DogPalApp/DogWalkerProfileCreationView.swift
@@ -0,0 +1,18 @@
+//
+//  DogWalkerProfileCreationView.swift
+//  DogPalApp
+//
+//  Created by User on 2024-10-29.
+//
+
+import SwiftUI
+
+struct DogWalkerProfileCreationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DogWalkerProfileCreationView()
+}

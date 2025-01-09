80a7b4895ca53c132d5ba5bafaee6f4504f590ca
Wandrey Haagensen <wandrey.wic@gmail.com>
2025-01-08 21:46:15 -0500

    comment working, but user info not showing. will try to copy function from user page

--- DogPalApp/ReviewRateView.swift
+++ DogPalApp/ReviewRateView.swift
@@ -34,132 +34,140 @@ struct ReviewRateView: View {
     ]
     
     var body: some View {
-       
-            ScrollView {
-                VStack {
-                    
-                    Image("DogPalLogo2")
-                        .resizable()
-                        .scaledToFit()
+        
+        ScrollView {
+            VStack {
+                
+                Image("DogPalLogo2")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                    .frame(width: 350, height: 150)
+                
+                Text("Park Reviews")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding()
+                
+                //testing textbox for response
+                Text("Name: \(userName)")
+                    .font(.title2)
+                    .bold()
+                Text("Idade: \(userAge)")
+                    .font(.title2)
+                    .bold()
+                
+                // Caixa de texto para o comentário
+                TextEditor(text: $commentText)
+                    .frame(height: 100)
+                    .padding()
+                    .background(Color(UIColor.systemGray6))
+                    .cornerRadius(10)
+                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 1))
+                    .padding(.horizontal)
+                
+                // Seletor de estrelas para avaliação
+                HStack {
+                    ForEach(1...5, id: \.self) { star in
+                        Image(systemName: star <= rating ? "star.fill" : "star")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(star <= rating ? .yellow : .gray)
+                            .onTapGesture {
+                                rating = star // Atualiza a nota
+                            }
+                    }
+                }
+                .padding()
+                
+                // Botão para salvar comentário
+                Button(action: saveComment) {
+                    Text("Save Comment")
                         .padding()
-                        .frame(width: 350, height: 150)
-                    
-                    Text("Park Reviews")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .padding()
-                    //testing textbox for response
-                    Text("Name: \(userName)")
-                        .font(.title2)
-                        .bold()
-                    Text("Idade: \(userAge)")
-                        .font(.title2)
-                        .bold()
-                    // Caixa de texto para o comentário
-                    TextEditor(text: $commentText)
-                        .frame(height: 100)
-                        .padding()
-                        .background(Color(UIColor.systemGray6))
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
                         .cornerRadius(10)
-                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 1))
                         .padding(.horizontal)
                     
-                    // Seletor de estrelas para avaliação
-                               HStack {
-                                   ForEach(1...5, id: \.self) { star in
-                                       Image(systemName: star <= rating ? "star.fill" : "star")
-                                           .resizable()
-                                           .frame(width: 30, height: 30)
-                                           .foregroundColor(star <= rating ? .yellow : .gray)
-                                           .onTapGesture {
-                                               rating = star // Atualiza a nota
-                                           }
-                                   }
-                               }
-                               .padding()
-
-                               // Botão para salvar comentário
-                               Button(action: saveComment) {
-                                   Text("Save Comment")
-                                       .padding()
-                                       .frame(maxWidth: .infinity)
-                                       .background(Color.blue)
-                                       .foregroundColor(.white)
-                                       .cornerRadius(10)
-                                       .padding(.horizontal)
-                                   Spacer()
-                                          }
-                                          .onAppear { fetchUserDetails() // Busca os detalhes do usuário ao carregar a view
-                                        }
-                    
-                    ForEach(parks) { park in
-                        VStack(alignment: .leading) {
-                            Text(park.name)
-                                .font(.headline)
-                                .padding(.bottom, 5)
-                            
-                            Text("Rating: \(park.rating, specifier: "%.1f")")
-                                .font(.subheadline)
-                                .foregroundColor(.secondary)
-                                .padding(.bottom, 10)
-                            
-                            ForEach(park.reviews) { review in
-                                ReviewCardView(review: review)
-                            }
-                            
-                            Divider().padding(.vertical)
-                        }
-                    }
-                    
-                   }
+                    Spacer()
                     
                 }
-                .padding()            }
+                
+                
+                ForEach(parks) { park in
+                    VStack(alignment: .leading) {
+                        Text(park.name)
+                            .font(.headline)
+                            .padding(.bottom, 5)
+                        
+                        Text("Rating: \(park.rating, specifier: "%.1f")")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                            .padding(.bottom, 10)
+                        
+                        ForEach(park.reviews) { review in
+                            ReviewCardView(review: review)
+                        }
+                        
+                        Divider().padding(.vertical)
+                    }
+                }
+                .onAppear { fetchUserDetails() // Busca os detalhes do usuário ao carregar a view
+                }
+            }
+        }
+        .padding()
+    }
+    
     
     // Função para salvar o comentário e a nota no Firebase
-       func saveComment() {
-           if let user = Auth.auth().currentUser {
-               let userId = user.uid
-               let commentData: [String: Any] = [
-                   "userName": userName,
-                   "userAge": userAge,
-                   "commentText": commentText,
-                   "rating": rating,
-                   "timestamp": Date().timeIntervalSince1970
-               ]
-
-               ref.child("comments").child(userId).childByAutoId().setValue(commentData) { error, _ in
-                   if let error = error {
-                       print("Erro ao salvar comentário: \(error.localizedDescription)")
-                   } else {
-                       print("Comentário salvo com sucesso!")
-                       // Limpa os campos após salvar
-                       commentText = ""
-                       rating = 0
-                   }
-               }
-           } else {
-               print("Nenhum usuário está logado.")
-           }
-       }
+    func saveComment() {
+        if let user = Auth.auth().currentUser {
+            let userId = user.uid
+            let commentData: [String: Any] = [
+                "userName": userName,
+                "userAge": userAge,
+                "commentText": commentText,
+                "rating": rating,
+                "timestamp": Date().timeIntervalSince1970
+            ]
+            
+            ref.child("comments").child(userId).childByAutoId().setValue(commentData) { error, _ in
+                if let error = error {
+                    print("Erro ao salvar comentário: \(error.localizedDescription)")
+                } else {
+                    print("Comentário salvo com sucesso!")
+                    // Limpa os campos após salvar
+                    commentText = ""
+                    rating = 0
+                }
+            }
+        } else {
+            print("Nenhum usuário está logado.")
+        }
+    }
     
     //funcao para carregar oo usuario
     func fetchUserDetails() {
-           if let user = Auth.auth().currentUser {
-               let userId = user.uid
-               ref.child("users").child(userId).observeSingleEvent(of: .value) { snapshot in
-                   if let value = snapshot.value as? [String: Any] {
-                       self.userName = value["userName"] as? String ?? "Desconhecido"
-                       self.userAge = value["userAge"] as? String ?? "N/A"
-                   }
-               }
-           } else {
-               print("Nenhum usuário está logado.")
-           }
-       }
-    
-    
+        
+        if let user = Auth.auth().currentUser {
+            let userId = user.uid
+            ref.child("users").child(userId).observeSingleEvent(of: .value) { snapshot in
+                if let value = snapshot.value as? [String: Any] {
+                    // Acessa diretamente "userName" e "userAge"
+                    self.userName = value["userName"] as? String ?? "Desconhecido"
+                    self.userAge = value["userAge"] as? String ?? "N/A"
+                    print("Dados carregados: userName: \(self.userName), userAge: \(self.userAge)")
+                } else {
+                    print("Snapshot vazio ou dados ausentes.")
+                }
+            } withCancel: { error in
+                print("Erro ao buscar dados do Firebase: \(error.localizedDescription)")
+            }
+        }
     }
+}
 
 struct ReviewCardView: View {
     var review: ParkReview
